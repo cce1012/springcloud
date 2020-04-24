2686e9e98d276be5f192d8f2fd491ceecfa77707
cce1012 <1064821011@qq.com>
2020-04-24 16:12:39 +0800

    first

---
+++ springcloud-provider-8001/src/main/java/com/cce/service/DeptService.java
@@ -0,0 +1,13 @@
+package com.cce.service;
+
+import java.util.List;
+
+import com.cce.entity.Dept;
+
+public interface DeptService {
+	public boolean add(Dept dept);
+
+    public Dept get(Long id);
+
+    public List<Dept> list();
+}
