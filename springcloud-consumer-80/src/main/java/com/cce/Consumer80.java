fe73a2883dedaae7f4b3ca09a8ebde396349bfba
cce1012 <1064821011@qq.com>
2020-04-26 15:05:43 +0800

    eureka

--- springcloud-consumer-80/src/main/java/com/cce/Consumer80.java
+++ springcloud-consumer-80/src/main/java/com/cce/Consumer80.java
@@ -2,12 +2,10 @@ package com.cce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class Provider8001 {
+public class Consumer80 {
 	public static void main(String[] args) {
-		SpringApplication.run(Provider8001.class, args);
+		SpringApplication.run(Consumer80.class, args);
 	}
 }
--- springcloud-eureka-7001/src/main/java/com/cce/EurekaServer7001.java
+++ springcloud-eureka-7001/src/main/java/com/cce/EurekaServer7001.java
@@ -2,12 +2,12 @@ package com.cce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class Provider8001 {
+@EnableEurekaServer
+public class EurekaServer7001 {
 	public static void main(String[] args) {
-		SpringApplication.run(Provider8001.class, args);
+		SpringApplication.run(EurekaServer7001.class, args);
 	}
 }
